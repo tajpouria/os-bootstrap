f7f2898481fed13d75c69babf280e62b7802ad72
tajpouria <tajpouria.dev@gmail.com>
2022-01-03 10:17:14 +0400

    feat: add ghget

--- ubuntu.sh
+++ ubuntu.sh
@@ -105,6 +105,14 @@ then
   sudo mv /tmp/kind /usr/local/bin
 fi
 
+if ! command -v ghget &> /dev/null
+then
+  echo '🚀 Placing ghget'
+  wget -O /tmp/ghget https://github.com/mohd-akram/ghget/raw/master/ghget
+  sudo chmod +x /tmp/ghget
+  sudo mv /tmp/ghget /usr/local/bin
+fi
+
 
 echo '📌 Tunning configurations'
 
