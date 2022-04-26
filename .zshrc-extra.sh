d089d774216bc9c784a169e988cb4f7a45493e70
tajpouria <tajpouria.dev@gmail.com>
2022-04-26 21:22:59 +0400

    update 2 files and create 3 files

--- .zshrc-extra.sh
+++ .zshrc-extra.sh
@@ -1,18 +1,8 @@
-export ZSH="/home/tajpouria/.oh-my-zsh"
-
-ZSH_THEME="robbyrussell"
-
-plugins=(
-    git
-    zsh-autosuggestions
-    kubectl
-)
-
 # zsh-autosuggestions
 bindkey '^ ' autosuggest-accept
 
 # zsh-syntax-highlighting
-source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # NVM
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
